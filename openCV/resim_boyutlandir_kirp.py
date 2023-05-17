10ca20af8971109923b073660391a270aebf8f59
Berat Karacaoğlu <brtkaracaoglu@gmail.com>
2023-05-17 12:53:25 +0300

    image resizing

---
+++ openCV/resim_boyutlandir_kirp.py
@@ -0,0 +1,23 @@
+import cv2
+
+#
+img = cv2.imread("safi.JPG",1)
+print("Resim Boyutu: ", img.shape)
+cv2.imshow("Orjinal", img)
+
+#resized boyutlandırma
+imgResized = cv2.resize(img, (800,800))
+print("Resized Img Shape: ", imgResized.shape)
+cv2.imshow("img Resized", imgResized)
+cv2.imwrite("safiresized.jpg", img)
+
+
+#kırpma
+
+imgCropped = img[:200,0:300] #height  width
+cv2.imshow("Kirpik Resim", imgCropped)
+
+
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
